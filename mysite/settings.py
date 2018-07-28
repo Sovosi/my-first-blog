97a4ee7a396265d1a6b02ffbd79118c6c96ef058
Sophie <sovosi@bluemail.ch>
2018-07-28 17:45:24 +0200

    Debugmodus deaktiviert

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hm5(#f*q83!r4&@69t7mf=x8508=*%ogspff#elyx2%#6oxe_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
